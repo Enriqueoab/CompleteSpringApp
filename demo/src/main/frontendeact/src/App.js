7b592c8ea4de570147b20bba42a09b03c330ea08
Enrique Barca <barcaenrique7@gmail.com>
2021-11-08 10:31:46 +0000

    Main function (Component) Update

--- demo/src/main/frontendeact/src/App.js
+++ demo/src/main/frontendeact/src/App.js
@@ -5,15 +5,30 @@ import axios from "axios";
 
 const UserProfiles = () => {
 
-  const fetchUserProfiles = () => {
+//Variables and where the variables are going to get their values
+const [userProfiles, setUserProfiles] = useState([]);
+
+const fetchUserProfiles = () => {
     axios.get("http://localhost:8080/api/v1/user-profile").then(res =>{console.log(res);
-    });
+    
+    const data = res.data;
+    //We get the variable state set it
+    setUserProfiles(data);
+  });
   }
+  //If there is any change in the array this function will be execute
   useEffect(() => {
     fetchUserProfiles();
 
   }, []);
-  return <h1>Hi</h1>
+  return userProfiles.map((userProfile,index) =>{
+      return (
+          <div key={index}>
+            <h1>{userProfile.username}</h1>
+            <p>{userProfile.userId}</p>
+          </div>
+      )
+  })  
 };
 
 function App() {
